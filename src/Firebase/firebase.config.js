df48870e6fb3dcafcade0b81cf33348a02a36179
nferdous2 <noorjahanferdous029@gmail.com>
2021-10-20 21:04:29 +0600

    firsts

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCanocuuTFyD68gw9n25NSZV9Qo93VJKyM",
-    authDomain: "health-service-7a903.firebaseapp.com",
-    projectId: "health-service-7a903",
-    storageBucket: "health-service-7a903.appspot.com",
-    messagingSenderId: "60252222301",
-    appId: "1:60252222301:web:ca5f920cce60a7c13faeb4"
+    apiKey: "AIzaSyD5uftyLbraybowK5iMfzMjVyMijy0qSd8",
+    authDomain: "health-web-b0254.firebaseapp.com",
+    projectId: "health-web-b0254",
+    storageBucket: "health-web-b0254.appspot.com",
+    messagingSenderId: "483592540875",
+    appId: "1:483592540875:web:8b7e0a95b366961c1d414e"
 };
 export default firebaseConfig;
